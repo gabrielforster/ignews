9950413507173556513b2f42b2c3aecd7d850cef
gabrielforster <rochafrgabriel@gmail.com>
2022-05-23 21:22:00 -0300

    feat: add global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default class MyDocument extends Document{
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" />
-          <title>ig.news</title>
         </Head>
         <body>
           <Main/>
