40b40d6f0ec76def8bb99ea18240f66454c4522e
gabrielforster <rochafrgabriel@gmail.com>
2022-05-24 23:18:32 -0300

    feat: init Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
-import styles from '../styles.module.scss'
+import styles from './styles.module.scss'
 
 export function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="Logo da aplicação" />
+                <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a>Home</a>
+                    <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
             </div>
