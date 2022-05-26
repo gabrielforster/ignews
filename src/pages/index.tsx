2126d832704ec2cc5549fa889abff01dea80e2e1
gabrielforster <rochafrgabriel@gmail.com>
2022-05-25 22:05:41 -0300

    feat: doing home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-
 import Head from 'next/head'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, Welcome</span>
+          <h1>News about the <span>React</span> World</h1>
+          <p>
+            Get access to all the content.<br />
+            <span>for $9.90/month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
