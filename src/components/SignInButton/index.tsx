53b462784ae5bd26cd3b7c8fa0cb6b36442db6ee
gabrielforster <rochafrgabriel@gmail.com>
2022-05-31 21:07:25 -0300

    feat: user auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss'
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
 
@@ -17,9 +17,10 @@ export function SignInButton() {
           <button
           type="button"
           className={styles.signInButton}
+          onClick={() => signOut()}
           >
             <FaGithub color="#04d361" />
-            Gabriel Forster
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
           </button>
       </>
